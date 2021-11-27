5ab65bf16fde52fc214f04e742d8dff183b2b14e
Alexander Cooper <thechosenwaffle@gmail.com>
2021-11-27 00:21:20 -0500

    Updates

---
+++ precache-manifest.ad1aa967d44f73fc591e79ffebb86515.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bceed694691a11e6302d62f782a1461",
+    "url": "/portfolio-acooper/index.html"
+  },
+  {
+    "revision": "0fcc8e737eb83d8b066b",
+    "url": "/portfolio-acooper/static/css/main.ed78a535.chunk.css"
+  },
+  {
+    "revision": "c0757831e19f84e99359",
+    "url": "/portfolio-acooper/static/js/2.00b49274.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/portfolio-acooper/static/js/2.00b49274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fcc8e737eb83d8b066b",
+    "url": "/portfolio-acooper/static/js/main.62e980d7.chunk.js"
+  },
+  {
+    "revision": "eab362c906625a28e63e",
+    "url": "/portfolio-acooper/static/js/runtime-main.1c7d35b1.js"
+  },
+  {
+    "revision": "7bdf83a4332ccc7837f83dec1a7e24e6",
+    "url": "/portfolio-acooper/static/media/api.7bdf83a4.jpg"
+  },
+  {
+    "revision": "74c8ec70ddf156ced4d6167f65dba626",
+    "url": "/portfolio-acooper/static/media/bootstrap-icon.74c8ec70.png"
+  },
+  {
+    "revision": "642561b3a98f709d1836c7f863f3841e",
+    "url": "/portfolio-acooper/static/media/discord.642561b3.jpg"
+  },
+  {
+    "revision": "0050b4e5f7be76fc4af520b4bbb85fdd",
+    "url": "/portfolio-acooper/static/media/express-icon.0050b4e5.png"
+  },
+  {
+    "revision": "8610ec11f37ca5e7b6f7ed7005d4da97",
+    "url": "/portfolio-acooper/static/media/flashcard.8610ec11.jpg"
+  },
+  {
+    "revision": "3fa494607a390fd0860a55f319c153b0",
+    "url": "/portfolio-acooper/static/media/github-icon-charcoal.3fa49460.png"
+  },
+  {
+    "revision": "5fdd1cc2699f1952ad56a25330c2a28c",
+    "url": "/portfolio-acooper/static/media/knex-icon.5fdd1cc2.png"
+  },
+  {
+    "revision": "3ac9bc913b240abea7994bfceed0ab35",
+    "url": "/portfolio-acooper/static/media/linkedin-icon-charcoal.3ac9bc91.png"
+  },
+  {
+    "revision": "682428ec72a939b7787a79dafc591632",
+    "url": "/portfolio-acooper/static/media/node-icon.682428ec.png"
+  },
+  {
+    "revision": "a937a9d8552b0054cb184a0fd1f4983f",
+    "url": "/portfolio-acooper/static/media/pomodoro.a937a9d8.jpg"
+  },
+  {
+    "revision": "eafad199e149c9847d309854c6092a7d",
+    "url": "/portfolio-acooper/static/media/postgres-icon.eafad199.png"
+  },
+  {
+    "revision": "e5001c5a691bc5a9db7500f0abbf603b",
+    "url": "/portfolio-acooper/static/media/react-icon.e5001c5a.png"
+  },
+  {
+    "revision": "6eaeb7aaa93e9c83531fa759a681c90c",
+    "url": "/portfolio-acooper/static/media/restaurant.6eaeb7aa.jpg"
+  },
+  {
+    "revision": "a71e460b60d14e3433858f09e62774be",
+    "url": "/portfolio-acooper/static/media/seedspark_me.a71e460b.png"
+  }
+]);
