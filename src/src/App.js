2da49b98999bd93395faec73a2c9d36d9142c192
Alexander Cooper <thechosenwaffle@gmail.com>
2021-11-27 00:20:22 -0500

    Update skills, fix missing imports and dependencies

--- src/src/App.js
+++ src/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import About from "./about/About";
 import Contact from "./contact/Contact";
 import Navbar from "./navigation/Navbar";
--- src/src/navigation/NavLink.js
+++ src/src/navigation/NavLink.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import capitalizeFirst from "../common/capitalizeFirst";
 
 import "../style.css";
